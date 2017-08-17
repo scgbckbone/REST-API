eaa3d037dc5c66b7f6c40087e9b1bab0c2b84668
scag <virgovica@gmail.com>
2017-08-17 23:37:17 +0200

    minor bug fixes no2

--- resources/users_alchemy.py
+++ resources/users_alchemy.py
@@ -1,7 +1,7 @@
 import sqlite3
 from flask_restful import Resource, reqparse
 from models.users_alchemy import UserModel
-from req_log import logger
+from .req_log import logger
 
 class User_register(Resource):
     parser = reqparse.RequestParser()
