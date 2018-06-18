521cee8cedcf8821fe9deddb501e38005ba3c4fb
andrej <andrej.virgovic@pipelinersales.com>
2018-06-18 18:14:59 +0200

    added cv;

--- app_alchemy.py
+++ app_alchemy.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from flask_jwt import JWT
 from flask_restful import Api
 
@@ -10,7 +10,7 @@ from resources.store_alchemy import Store, StoreList
 from resources.contacts_resource import Contact, ContactList
 from resources.error_handlers import api_errors
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=".", static_folder="assets")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///DATA.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = 'andrej2154'
@@ -25,6 +25,11 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(User_register, '/register')
 api.add_resource(Contact, "/call_me/<string:name>")
 api.add_resource(ContactList, "/contacts")
+
+
+@app.route("/", methods=["GET"])
+def render_cv():
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
