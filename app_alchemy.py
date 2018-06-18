6c7f26b738e0d6ad4f89d638544716be15f95df7
Andrej Virgovic <andrej@scagbackbone.localdomain>
2018-06-18 16:17:00 +0000

    dssf

--- app_alchemy.py
+++ app_alchemy.py
@@ -9,6 +9,7 @@ from security_alchemy import authenticate, identity
 from resources.store_alchemy import Store, StoreList
 from resources.contacts_resource import Contact, ContactList
 from resources.error_handlers import api_errors
+from resources.index_resource import Index
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///DATA.db")
@@ -25,7 +26,7 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(User_register, '/register')
 api.add_resource(Contact, "/call_me/<string:name>")
 api.add_resource(ContactList, "/contacts")
-
+api.add_resource(Index, "/")
 
 if __name__ == '__main__':
     from object_SQLAlchemy import db
