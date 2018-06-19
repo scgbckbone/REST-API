196cbd4bb5ccf2fa70e493013e6c8940bb6cb084
andrej <andrej.virgovic@pipelinersales.com>
2018-06-19 23:04:34 +0200

    extended password hash lenght in databse (column definition);

--- models/users_alchemy.py
+++ models/users_alchemy.py
@@ -8,7 +8,7 @@ class UserModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80))
-    password = db.Column(db.String(80))
+    password = db.Column(db.String(120))
 
     def __init__(self, username, password):
         self.username = username
